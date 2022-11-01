c1fda5154320aca9664798b870fa84beaee7ddd9
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-11-01 11:33:21 -0700

    Updated working for step A7

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -10,13 +10,13 @@ class RecipeCard extends HTMLElement {
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    // A4. TODO - Insert all of the styles in to the <style> element
+    // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
   }
 
   /**
    * Called when the .data property is set on this element.
-   * 
+   *
    * For Example:
    * let recipeCard = document.createElement('recipe-card'); // Calls constructor()
    * recipeCard.data = { foo: 'bar' } // Calls set data({ foo: 'bar' })
@@ -40,9 +40,10 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    // A7. TODO - Set the contents of the <article> with the <recipe-card> template and
-    //           the data passed in. You should use Template literals (tempalte strings)
-    //           and element.innerHTML for this.
+    // A7. TODO - Set the contents of the <article> with the <article> template given in
+    //           cardTemplate.html and the data passed in (You should only have one <article>,
+    //           do not nest an <article> inside another <article>). You should use Template
+    //           literals (tempalte strings) and element.innerHTML for this.
   }
 }
 
